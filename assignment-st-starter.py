0f7016df874f0a5b5fc62e726d88e9a1680b2781
Frankiesummer <144880400+19kkkyu@users.noreply.github.com>
2024-09-25 20:50:24 +0800

    Update assignment-st-starter.py

--- assignment-st-starter.py
+++ assignment-st-starter.py
@@ -15,6 +15,7 @@ st.write(df)
 # you need to set the x labels and y labels
 # a sample diagram is shown below
 fig, ax = plt.subplots(1, 3, figsize=(15, 5))
+plt.style.use('seaborn-v0_8')
 for i, pclass in enumerate(sorted(df['Pclass'].unique()), start=0):
     df[df['Pclass'] == pclass].boxplot(column='Fare', ax=ax[i])
     ax[i].set_xlabel(f'PClass = {pclass}')
